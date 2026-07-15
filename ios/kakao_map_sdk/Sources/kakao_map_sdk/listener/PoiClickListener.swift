090680709e71977c9c0140a9aedb22f897a9cbc3
chanmi <cksal1052@ewha.ac.kr>
2026-07-16 06:12:48 +0900

    debug: add temporary print statements to trace iOS POI click flow

--- ios/kakao_map_sdk/Sources/kakao_map_sdk/listener/PoiClickListener.swift
+++ ios/kakao_map_sdk/Sources/kakao_map_sdk/listener/PoiClickListener.swift
@@ -11,6 +11,7 @@ class PoiClickListener {
     }
 
     func onPoiInteractionEvent(_ param: PoiInteractionEventParam) {
+        print("[HISSPOT_DEBUG] onPoiInteractionEvent fired, enable=\(enable), poiId=\(param.poiItem.itemID)")
         if enable {
             channel.invokeMethod("onPoiClick", arguments: [
                 "layerId": param.poiItem.layerID,
